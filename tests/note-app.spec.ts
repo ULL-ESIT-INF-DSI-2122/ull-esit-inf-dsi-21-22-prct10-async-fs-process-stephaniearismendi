6a2d68fc7ccb0575b5788361736061dc719034ff
Stephanie Arismendi <alu0101351728@ull.edu.es>
2022-04-26 17:50:53 +0000

    preparacion modificacion

--- tests/note-app.spec.ts
+++ tests/note-app.spec.ts
@@ -1,9 +1,9 @@
 import {expect} from 'chai';
 import 'mocha';
-
+import {helloWorld} from '../src/ejercicio-clase/modificacion';
 
 describe('Clase AddMapReduce', () => {
   it('Debe existir una clase AddMapReduce', () => {
-    expect().to.exist;
+    expect(helloWorld()).to.exist;
   });
 });
