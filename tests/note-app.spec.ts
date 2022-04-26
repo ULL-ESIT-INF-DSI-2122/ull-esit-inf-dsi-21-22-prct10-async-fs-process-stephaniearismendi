433e6b6e9672904a83a9244afdeed50d8a855d44
Stephanie Arismendi <alu0101351728@ull.edu.es>
2022-04-26 18:07:50 +0000

    set entorno

--- tests/note-app.spec.ts
+++
@@ -1,9 +0,0 @@
-import {expect} from 'chai';
-import 'mocha';
-import {helloWorld} from '../src/ejercicio-clase/modificacion';
-
-describe('Clase AddMapReduce', () => {
-  it('Debe existir una clase AddMapReduce', () => {
-    expect(helloWorld()).to.exist;
-  });
-});
