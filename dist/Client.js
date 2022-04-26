4b2f71b94a332f512589ea45b0ce6872557cbc6b
Stephanie Arismendi <alu0101351728@ull.edu.es>
2022-04-26 17:42:08 +0000

    sonar

--- dist/Client.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const reduce_1 = require("./reduce");
-funcion;
-clientCode(reduce, reduce_1.Reduce);
-{
-    reduce.run();
-}
