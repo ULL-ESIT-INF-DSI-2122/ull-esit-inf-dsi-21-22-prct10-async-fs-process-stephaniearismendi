cbbc1c9be19f3798e0ffcd3382fcec5b18da31c5
Stephanie Arismendi <alu0101351728@ull.edu.es>
2022-04-27 10:19:04 +0000

    modificacion

--- tests/modificacion.spec.ts
+++
@@ -1,9 +0,0 @@
-import {expect} from 'chai';
-import 'mocha';
-// import {helloWorld} from '../src/ejercicio-clase/modificacion';
-
-// describe('Test clases', () => {
-//   it('Debe existir', () => {
-//     expect(helloWorld()).to.not.throw;
-//   });
-// });
